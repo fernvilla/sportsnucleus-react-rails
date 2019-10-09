d46db52b2de7f912677de7299e6f89d96652bf97
Fernando Villasenor <fvilla18@gmail.com>
2019-10-08 23:05:29 -0700

    Create basic site layout

---
+++ client/src/components/SiteLayout.js
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import { Layout, Menu, Breadcrumb, Icon } from "antd";
+
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
+
+const SiteLayout = props => {
+  const [collapsed, setCollapsed] = useState(false);
+
+  const onCollapse = collapsed => {
+    console.log(collapsed);
+    setCollapsed(collapsed);
+  };
+
+  return (
+    <Layout style={{ minHeight: "100vh" }}>
+      <Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
+        <div style={{ textAlign: "center" }}>
+          <img
+            src={`${process.env.PUBLIC_URL}${
+              collapsed ? "/images/logo-transparent-solo.png" : "/images/logo-transparent.png"
+            }`}
+            alt="Logo"
+            style={{
+              height: "25px",
+              margin: "16px 0"
+            }}
+          />
+        </div>
+
+        <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
+          <Menu.Item key="1">
+            <Icon type="pie-chart" />
+            <span>Option 1</span>
+          </Menu.Item>
+          <Menu.Item key="2">
+            <Icon type="desktop" />
+            <span>Option 2</span>
+          </Menu.Item>
+          <SubMenu
+            key="sub1"
+            title={
+              <span>
+                <Icon type="user" />
+                <span>User</span>
+              </span>
+            }
+          >
+            <Menu.Item key="3">Tom</Menu.Item>
+            <Menu.Item key="4">Bill</Menu.Item>
+            <Menu.Item key="5">Alex</Menu.Item>
+          </SubMenu>
+          <SubMenu
+            key="sub2"
+            title={
+              <span>
+                <Icon type="team" />
+                <span>Team</span>
+              </span>
+            }
+          >
+            <Menu.Item key="6">Team 1</Menu.Item>
+            <Menu.Item key="8">Team 2</Menu.Item>
+          </SubMenu>
+          <Menu.Item key="9">
+            <Icon type="file" />
+            <span>File</span>
+          </Menu.Item>
+        </Menu>
+      </Sider>
+
+      <Layout>
+        <Content style={{ margin: "16px" }}>{props.children}</Content>
+        <Footer style={{ textAlign: "center" }}>Sports Nucleus ©{new Date().getFullYear()}</Footer>
+      </Layout>
+    </Layout>
+  );
+};
+
+export default SiteLayout;
